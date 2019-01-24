8f230bee4a2167610fb5a818cd2bfd1b3d1044b9
kamronbek <osimjonov2001@gmail.com>
2019-01-24 11:35:42 +0500

    Initial Commit

---
+++ videosaver/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  videosaver
+//
+//  Created by Kamronbek on 1/24/19.
+//  Copyright © 2019 Kamronbek. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
