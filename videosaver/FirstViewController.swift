4b2888a571c333a1ac95724e683e4714a51ca169
kamronbek <osimjonov2001@gmail.com>
2019-01-29 21:34:58 +0500

    first commit

--- videosaver/FirstViewController.swift
+++ videosaver/FirstViewController.swift
@@ -9,6 +9,42 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBOutlet weak var setlinkTextField: UITextField!
+    @IBOutlet weak var DownloadAction: UILabel!
+    @IBAction func DownloadUIButton(_ sender: UIButton) {
+        DownloadAction.text = "Скачивание началось"
+//        UIApplication.shared.keyWindow!.endEditing(true)
+        
+//        let urlString = "http://jsonplaceholder.typicode.com/users/1"
+//        guard let requestUrl = URL(string:urlString) else { return }
+//        let request = URLRequest(url:requestUrl)
+//        let _ = URLSession.shared.dataTask(with: request) {
+//            (data, response, error) in
+//            self.DownloadAction.text = "Скачивание 1111"
+//            if error == nil,let usableData = data {
+//                print(usableData) //JSONSerialization
+//            }
+//        }
+
+        
+        let urlString = URL(string: "https://jsonplaceholder.typicode.com/users/1")
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) {
+                (data, response, error) in
+                if error != nil {
+                    self.DownloadAction.text = "error"
+                } else {
+                    self.DownloadAction.text = "done"
+                    if let data = data {
+                        if let stringData = String(data: data, encoding: String.Encoding.utf8) {
+                            self.DownloadAction.text = stringData
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +53,3 @@ class FirstViewController: UIViewController {
 
 
 }
-
