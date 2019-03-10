5e7dec89e94dbc6eec72690b35dfe7a732e61b5f
kamronbek <osimjonov2001@gmail.com>
2019-03-10 17:58:46 +0500

    новый дизайн, исчезает клавиатура при тапе

--- videosaver/FirstViewController.swift
+++ videosaver/FirstViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 import Foundation
 import Photos
 
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
 class FirstViewController: UIViewController {
     
     @IBOutlet weak var setlink: UITextField!
     @IBOutlet weak var DownloadAction: UILabel!
+    
+    
+    // tap to hide keyboard
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    override func dismissKeyboard() {
+        view.endEditing(true)
+    }
     
     func matches(for regex: String, in text: String) -> [String] {
         
@@ -29,7 +55,7 @@ class FirstViewController: UIViewController {
             return []
         }
     }
-    
+
     @IBAction func button(_ sender: Any) {
         if setlink.text == "" {
             self.DownloadAction.text = "Вставьте ссылку для скачивания"
@@ -54,29 +80,21 @@ class FirstViewController: UIViewController {
             do {
                 let myHTMLString = try String(contentsOf: myURL as URL)
 
-//                let matched = matches(for: "video_url\":\"(.*mp4)\"", in: myHTMLString)
-
                 let matched = matches(for: "video_url\":\"(.*mp4).*\",\"video_view_count", in: myHTMLString)
-                print(matched)
+                
                 let matched_1_replace = matched[0].replacingOccurrences(of: "video_url\":\"", with: "")
                 
                 let matched_2_replace = matched_1_replace.replacingOccurrences(of: "\",\"video_view_count", with: "")
-                print(matched_2_replace)
+                
                 
                 DispatchQueue.global(qos: .userInteractive).async {
-                    print("11111111")
                     if let url = URL(string: matched_2_replace),
                         let urlData = NSData(contentsOf: url) {
-                            print("33333333")
                             let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0];
-                            print("4444444")
                             let filePath="\(documentsPath)/tempFile.mp4"
-                            print("5555555")
                             DispatchQueue.main.async {
-                                print("6666666")
                                 urlData.write(toFile: filePath, atomically: true)
                                 PHPhotoLibrary.shared().performChanges({
-                                    print("88888888")
                                     PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: URL(fileURLWithPath: filePath))
                                 }) { completed, error in
                                     if completed {
